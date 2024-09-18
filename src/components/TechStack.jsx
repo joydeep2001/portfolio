84cfcf9f71740e7a7e5a734bd880630cc2813eb1
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-09-18 21:28:35 +0530

    techstack: Add techstack with horizontal scroll.

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React, { useLayoutEffect } from "react";
+import gsap from "gsap";
 
 function TechStack() {
+  
+  useLayoutEffect(() => {
+    gsap.to(document.querySelector(".techstack .container"), {
+      x: -2200,
+      scrollTrigger: {
+        trigger: ".techstack .container",
+        start: "top 28%",
+        end: "+=2200",
+        scrub: true,
+        markers: true,
+
+        onEnter: () => {
+          console.log("starts");
+          document.querySelector(".techstack").style.position = "fixed";
+          document.querySelector(".techstack").style.top = "20%";
+          
+        },
+        onLeave: () => {
+          console.log("leave");
+          document.querySelector(".techstack").style.position = "relative";
+        },
+        onLeaveBack: () => {
+          console.log("leave back");
+          document.querySelector(".techstack").style.position = "relative";
+        },
+      },
+    });
+  });
   return (
-    <div className='techstack'>TechStack</div>
-  )
+    <section className="techstack">
+      <h1>TechStack</h1>
+      <div className="container">
+        <div className="img-cont">
+          <img alt="html" src="/portfolio/html.png" />
+        </div>
+        <div className="img-cont">
+          <img alt="css" src="/portfolio/css.png" />
+        </div>
+        <div className="img-cont">
+          <img alt="html" src="/portfolio/js.png" />
+        </div>
+        <div className="img-cont">
+          <img alt="html" src="/portfolio/react.png" />
+        </div>
+        <div className="img-cont">
+          <img alt="html" src="/portfolio/nodejs.png" />
+        </div>
+        <div className="img-cont">
+          <img alt="html" src="/portfolio/express-js.png" />
+        </div>
+        <div className="img-cont">
+          <img alt="html" src="/portfolio/mongodb.png" />
+        </div>
+        <div className="img-cont">
+          <img alt="html" src="/portfolio/ts.png.png" />
+        </div>
+        <div className="img-cont">
+          <img alt="html" src="/portfolio/html.png" />
+        </div>
+        <div className="img-cont">
+          <img alt="html" src="/portfolio/html.png" />
+        </div>
+        <div className="img-cont">
+          <img alt="html" src="/portfolio/html.png" />
+        </div>
+        <div className="img-cont">
+          <img alt="html" src="/portfolio/html.png" />
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default TechStack
+export default TechStack;
