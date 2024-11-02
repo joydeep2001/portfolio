f05e519b50c9787f4ab0f30b76778552b6cd2565
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-11-02 20:26:08 +0530

    TechStack: Update icons.

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,64 +1,92 @@
 import React, { useLayoutEffect } from "react";
 import gsap from "gsap";
 
+const techIcons = [
+  {
+    name: "HTML",
+    icon: "tech-icons/HTML5.png",
+  },
+  {
+    name: "CSS",
+    icon: "tech-icons/CSS3.png",
+  },
+  {
+    name: "JavaScript",
+    icon: "tech-icons/JavaScript.png",
+  },
+  {
+    name: "React",
+    icon: "tech-icons/React.png",
+  },
+  {
+    name: "Next.js",
+    icon: "tech-icons/Next.js.png",
+  },
+  {
+    name: "Node.js",
+    icon: "tech-icons/Node.js.png",
+  },
+  {
+    name: "Node.js",
+    icon: "tech-icons/Express.png",
+  },
+  {
+    name: "Flutter",
+    icon: "tech-icons/Flutter.png",
+  },
+  {
+    name: "MySQL",
+    icon: "tech-icons/MySQL.png",
+  },
+  {
+    name: "NGNIX",
+    icon: "tech-icons/NGINX.png",
+  },
+  {
+    name: "Git",
+    icon: "tech-icons/Git.png",
+  },
+  {
+    name: "MongoDB",
+    icon: "tech-icons/MongoDB.png",
+  },
+  {
+    name: "Rust",
+    icon: "tech-icons/Rust.png",
+  },
+  {
+    name: "Actix",
+    icon: "tech-icons/Actix.png",
+  },
+];
+
 function TechStack() {
-  
   useLayoutEffect(() => {
     const ctx = gsap.context(() => {
       gsap.to(document.querySelector(".techstack .container"), {
-        x: -2200,
+        x: -3260,
         scrollTrigger: {
           trigger: ".techstack",
           start: "top 20%",
-          end: "+=2200",
+          end: "+=3260",
           scrub: true,
           markers: true,
           pin: true,
         },
       });
-    })
+    });
     return () => ctx.revert();
   }, []);
   return (
     <section className="techstack">
       <h1>TechStack</h1>
+
       <div className="container">
-        <div className="img-cont">
-          <img alt="html" src="/portfolio/html.png" />
-        </div>
-        <div className="img-cont">
-          <img alt="css" src="/portfolio/css.png" />
-        </div>
-        <div className="img-cont">
-          <img alt="html" src="/portfolio/js.png" />
-        </div>
-        <div className="img-cont">
-          <img alt="html" src="/portfolio/react.png" />
-        </div>
-        <div className="img-cont">
-          <img alt="html" src="/portfolio/nodejs.png" />
-        </div>
-        <div className="img-cont">
-          <img alt="html" src="/portfolio/express-js.png" />
-        </div>
-        <div className="img-cont">
-          <img alt="html" src="/portfolio/mongodb.png" />
-        </div>
-        <div className="img-cont">
-          <img alt="html" src="/portfolio/ts.png.png" />
-        </div>
-        <div className="img-cont">
-          <img alt="html" src="/portfolio/html.png" />
-        </div>
-        <div className="img-cont">
-          <img alt="html" src="/portfolio/html.png" />
-        </div>
-        <div className="img-cont">
-          <img alt="html" src="/portfolio/html.png" />
-        </div>
-        <div className="img-cont">
-          <img alt="html" src="/portfolio/html.png" />
-        </div>
+        {techIcons.map(({ name, icon }) => (
+          <div title={name} className="img-cont">
+            <img alt="html" src={`/portfolio/${icon}`} />
+          </div>
+        ))}
       </div>
     </section>
   );
